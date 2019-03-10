73c380b4ec7e765400438c3671a7d5d64ca08895
Arav Narula <proteximportsexports@gmail.com>
2019-03-10 14:56:57 -0400

    Create fool1.js

---
+++ levels/fool1.js
@@ -0,0 +1,24 @@
+alert(Your Coumpter has seemed to find risks. Standby Open your console log ctrl + shift + J!");
+wait(0.1);
+wait(0.1);
+wait(0.1);
+
+      
+      var risk = Math.random();
+      
+      
+      
+      wait(10);
+      
+      console.warn("Standby, Checking Virus/Risks/");
+      
+      wait(5);
+      
+      console.log("No Cirtcal Alerts. Please Standy ...")
+      
+      wait(3);
+      
+      console.error( risk + "Found")
+      alert("Call 0000-0000-000-000-000-00, NOW!")
+      
+      /// End of Level 2
